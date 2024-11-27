37770a35922f6fad79036a495fc8ecbcd50be1c7
Osama Rabea <osrab3@gmail.com>
2024-11-27 09:31:47 +0200

    data as json

---
+++ data/courses.js
@@ -0,0 +1,15 @@
+let courses = [
+    {
+        id: 1,
+        name: "js",
+        time: 15
+    },
+    {
+        id: 2,
+        name: "react",
+        time: 20
+    }
+];
+
+
+module.exports = courses;
