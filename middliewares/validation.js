fe9745c717bdcca9b3740b3a24b72dda0c6d53fd
Osama Rabea <osrab3@gmail.com>
2024-11-28 22:58:28 +0200

    fix

--- middliewares/validation.js
+++
@@ -1,9 +0,0 @@
-const { body} = require('express-validator');
-const validationSchme = () => {
-    return [
-        body("name").notEmpty().withMessage("Name is required"),
-        body("time").notEmpty().isNumeric().withMessage("Time must be a number")
-    ]
-};
-
-module.exports ={ validationSchme};
